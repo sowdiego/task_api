bfd5d7118572b06a5d047c2d9aaeb83fe42f1396
msow3011 <mamsow@dgid.sn>
2025-07-11 18:19:44 +0000

    mise en prod

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,17 @@
 //dd('api.php exécuté');
 
 use App\Http\Controllers\Api\TaskController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('tasks', TaskController::class);
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API OK']);
+});
+
+Route::get('/run-migrations', function () {
+    // ATTENTION : à supprimer après usage !
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migrations exécutées avec succès !';
 });
